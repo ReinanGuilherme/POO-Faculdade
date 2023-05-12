7ebad1e7b034809724659ee3f3bd8cae95e9a7e3
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.local>
2023-05-12 17:24:36 -0300

    first commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,70 @@
+﻿namespace POO_TransacoesBanco
+{
+    public class ContaBancaria
+    {
+        public string Nome { get;  }
+        public string Cpf { get;  }
+        public int NumConta { get;  }
+        public int NumAgencia { get;  }
+        public float Saldo { get; private set; }
+        public TipoConta Tipo { get;  }
+
+        public ContaBancaria(string nome, string cpf, TipoConta tipo)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Tipo = tipo;
+            Saldo = 0;
+
+
+            Random randNum = new Random();
+            NumAgencia = randNum.Next(1000, 9999);
+            NumAgencia = randNum.Next(100, 999);
+        }
+
+        public void Depositar(float valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor de depósito deve ser maior que zero.");
+            }
+            else
+            {
+                Saldo += valor;
+            }
+        }
+
+        public void Sacar(float valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor de saque deve ser maior que zero.");
+            }
+            else if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para saque.");
+            }
+            else
+            {
+                Saldo -= valor;
+            }
+        }
+
+        public void Transferir(float valor, ContaBancaria contaDestino)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor de transferência deve ser maior que zero.");
+            }
+            else if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para transferência.");
+            }
+            else
+            {
+                Saldo -= valor;
+                contaDestino.Depositar(valor);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using POO_TransacoesBanco;
+
+
+
+ContaBancaria pessoa1 = new ContaBancaria("Reinan O Lindo", "111.111.111-43", TipoConta.ContaCorrente);
+
+ContaBancaria pessoa2 = new ContaBancaria("Pessoa não importante", "222.222.222-86", TipoConta.ContaCorrente);
+
+pessoa1.Depositar(1000);
+pessoa2.Depositar(500);
+
+Console.WriteLine("Saldo da Pessoa 1: " + pessoa1.Saldo);
+Console.WriteLine("Saldo da Pessoa 2: " + pessoa2.Saldo);
+
+pessoa1.Transferir(500, pessoa2);
+
+Console.WriteLine("Saldo da Pessoa 1 após transferência: " + pessoa1.Saldo);
+Console.WriteLine("Saldo da Pessoa 2 após transferência: " + pessoa2.Saldo);
+
+pessoa2.Sacar(200);
+
+Console.WriteLine("Saldo da Pessoa 2 após saque: " + pessoa2.Saldo);
+Console.WriteLine("------------RESUMO------------");
+Console.WriteLine("Saldo da Pessoa 1: " + pessoa1.Saldo);
+Console.WriteLine("Saldo da Pessoa 2: " + pessoa2.Saldo);
+
+Console.ReadLine();
